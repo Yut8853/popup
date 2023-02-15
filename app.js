d78ffbcb3789562d690bb7499bf29852423b75c9
yk0611 <hello@junkbranding.com>
2023-02-15 16:49:24 +0900

    完成

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 const button = document.querySelector('.button')
+
+const popUp = document.querySelector('.popup-container')
+button.addEventListener('click', () => {
+  popUp.style.display = 'block'
+})
+
+const popUpCloseButton = document.querySelector('.popup-close-button');
+popUpCloseButton.addEventListener('click', () => {
+  popUp.style.display = 'none'
+})
+
+popUp.addEventListener('click', () => {
+  popUp.style.display = 'none'
+})
+
+const popInner = document.querySelector('.popup-box')
+popInner.addEventListener('click', (e) => {
+  e.stopPropagation()
+})
